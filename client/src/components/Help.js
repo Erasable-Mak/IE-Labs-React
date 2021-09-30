eea955aa162febf7cc1d1f6e060ab30768ff977e
Erasable-MAK <60771750+Erasable-Mak@users.noreply.github.com>
2021-09-30 17:09:29 +0530

    Add files via upload

---
+++ client/src/components/Help.js
@@ -0,0 +1,30 @@
+/* eslint-disable jsx-a11y/anchor-has-content */
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React from 'react'
+import Helmet from 'react-helmet'
+
+const Help = () => {
+    return (
+        <>
+            <Helmet bodyAttributes={{style: ' background-color: #00003B;'}}/>
+            <div className="tot">
+            <div className="container emp-profile">
+                <form method="">
+                    <div className="row">
+
+                        <div className="col-md-6">
+                           <div className="profile-head">
+                               <h5>Under Development</h5><br/>
+                               <h6>Under Development</h6><br/>
+                           </div>
+                        </div>
+                    </div>
+                </form>
+            </div>
+          </div> 
+        </>
+    )
+}
+
+
+export default Help
